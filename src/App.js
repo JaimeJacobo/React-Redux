8c96584578c2d50c25cb095365e7b782fe268a6f
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-04-01 16:29:16 +0200

    subiendo ejercicio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// 13. Importar useSelector y useDispatch de 'react-redux'
+import {useSelector, useDispatch} from 'react-redux';
+
+
 
 function App() {
+
+  //14. Llamar al estado de Redux y guardarlo en este componente con el nombre de 'stateFromRedux'
+  const stateFromRedux = useSelector((state) => state)
+
+  //15. Crear una variable que llame al useDispatch()
+  const dispatch = useDispatch()
+
+  //16. Crear las acciones que quiero llevar a cabo en este componente
+  const increment = ()=>{
+    return {
+      type: 'INCREMENT'
+    }
+  }
+  
+  const decrement = ()=>{
+    return {
+      type: 'DECREMENT'
+    }
+  }
+
+  //17. Conectar los dispatch de las acciones con los botones
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter: {stateFromRedux.counter}</h1>
+      <button onClick={()=>dispatch(increment())}>SUMAR 1</button>
+      <button onClick={()=>dispatch(decrement())}>RESTAR 1</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,75 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//1. Instalar redux y react-redux en la terminal con npm
+//2. Importar createStroe de 'redux'
+import { createStore } from 'redux'
+//11. Importar Provider de 'react-redux'
+import {Provider} from 'react-redux'
 
+//3. Crear mi "estado" local
+const myState = {
+  counter: 0
+}
+
+//4. Crear el reducer (embudo)
+//REDUCER
+const counterReducer = (state = myState, action)=>{
+  //9. Crear la logica para que el reducer haga una cosa y otra dependiendo de la accion que esta recibiendo
+  if(action.type === 'INCREMENT'){
+    return {
+      ...store,
+      counter: state.counter + 1
+    }
+  } else if(action.type === 'DECREMENT'){
+    return {
+      ...store,
+      counter: state.counter -1
+    }
+  }
+  return state;
+}
+
+//5. Crear nuestro store/almacen
+//STORE
+
+//10. Añadir la extension de Redux DevTools
+let store = createStore(counterReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+//6. Crear las acciones de increment y decrement
+//ACTIONS
+
+// const increment = ()=>{
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+
+// const decrement = ()=>{
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
+
+//8. Crear las subscribtions
+//SUBSCRIPTIONS
+
+store.subscribe(()=>console.log(store.getState().counter))
+
+
+//7. Activar las acciones con dispatch
+//ACTIONS DISPATCH
+// store.dispatch(increment())
+// store.dispatch(decrement())
+
+
+
+//12. Envolver App con el Provider y pasarle el store como argumento
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />      
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
